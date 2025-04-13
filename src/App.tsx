35ba0b925aebcad020aef884e3f9bb8e81522474
Andy <andy_garlop4@hotmail.com>
2025-04-13 11:11:42 -0600

    Creacion de nuevo proyecto de menu y propinas

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="p-5 bg-blue-600/60">
+        <div className="mx-auto max-w-4xl">
+          <h1 className="text-center text-2xl md:text-4xl font-bold text-white uppercase">Consumo y Propinas</h1>
+        </div>
+      </header>
+
+      <section className="mx-auto max-w-4xl">
+        <div className="flex justify-between items-center my-5">
+
+          <div>
+            <h1>Menu</h1>
+          </div>
+
+
+          <div>
+            <h2>Consumo</h2>
+          </div>
+
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default App
